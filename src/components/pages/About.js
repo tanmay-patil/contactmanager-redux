2cadc7dc5992bd6236e69b6b1547b13b34e3654b
Tanmay Patil <tanmayrmfc@gmail.com>
2018-11-11 16:26:13 +0530

    initial

---
+++ src/components/pages/About.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            <h1 className="display-4">About Contact Manager</h1>
+            <p className="lead">
+                Simple app created by Tanmay Patil to manage contacts
+            </p>
+            <p className="text-secondary">Version 1.0.0</p>
+            <p className="text-secondary">
+                - This is a React JS based application designed using Bootstrap
+                4.
+            </p>
+            <p className="text-secondary">
+                - This application makes use of JSON placeholder API to
+                playaround with temporary data and perform requests.
+            </p>
+            <p className="text-secondary">
+                - This application makes use of Redux which is an alternative to
+                Context API
+            </p>
+            <p className="text-secondary">
+                - I have another Contact Manager application which is created
+                using Context API instead of context API so don't forget to
+                check my github account
+            </p>
+        </div>
+    );
+};
